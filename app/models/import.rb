4440ebe03e4ee29cbc743a8e2d8e0f5084f4401e
Nam Chiem <117373842+nc172@users.noreply.github.com>
2026-06-08 10:38:48 -0400

    Add file upload support

--- app/models/import.rb
+++ app/models/import.rb
@@ -2,6 +2,8 @@ class Import < ApplicationRecord
   belongs_to :user
   has_many :trades, dependent: :destroy
 
+  has_one_attached :file
+
   validates :source_platform, presence: true
   validates :status, presence: true
 end
